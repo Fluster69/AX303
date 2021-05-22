848fc0e361cad3f561cc492107a1b43201e7b380
ertreerr <miguel26wang@gmail.com>
2021-05-22 17:01:03 +0800

    32131

--- Space Invaders - 9/main.js
+++ Space Invaders - 9/main.js
@@ -1,35 +1,11 @@
 $(document).ready(function(){
-  /*
-  ***************
-  *  Variables  *
-  ***************
-  Here are the global variables we'll use in our game.
-  */
-
-  // Variable for overall set up
   var screen, invaders_image, gameOver = false, win = false;
-
-  // Variables for frames to control screen's updates
   var frames, levelFrame, motion;
-
-  // Variables for the sprites
   var alienSprite, tankSprite, citySprite;
-
-  // Variables for storing the game objects
   var aliens, tank, cities, bullets;
-
-  // Variable for game control
   var alien_direction, keyPressed = [];
 
-  /*
-  ****************************
-  *  Main Function - main()  *
-  ****************************
-  The main() function is the main entry point to run our game.
-  */
-
   function main(){
-    // Repeatedly loop and update the game & draw the result on the screen
     let loop = function(){
       update();
       draw();
@@ -42,16 +18,6 @@ $(document).ready(function(){
       }
     };
   }
-
-  /*
-
-  **************************************
-  *  Initialization Function - init()  *
-  **************************************
-  init() function helps us initialize and start off our game 
-  by preparing the sprites we need and put them in the right positions.
-  Once the sprite is loaded, we can the main() function to start the main loop!
-  */
 
   function init(){
     if(screen == null){
@@ -67,125 +33,93 @@ $(document).ready(function(){
     invaders_image = new Image();
     invaders_image_src = "./assets/invaders.png";
 
-    // On image load, split the spritesheet into different sprites we want
     $(invaders_image).on("load", function(){
       alienSprite = [
-        [new Sprite(),new Sprite()],
-        [new Sprite(),new Sprite()],
-        [new Sprite(),new Sprite()]
+        [new Sprite(this, 0, 0, 22, 16),new Sprite(this, 0, 16, 22, 16)],
+        [new Sprite(this, 22, 0, 16, 16),new Sprite(this, 22, 16, 16, 16)],
+        [new Sprite(this, 38, 0, 24, 16),new Sprite(38, 16, 24, 16)]
       ];
+      tankSprite = new Sprite(this, 62, 0, 22, 16);
+      citySprite = new Sprite(this, 84, 8, 36, 24);
     });
+    tank = {
+      sprite: tankSprite, 
+      x: (screen.width - tankSprite.width) / 2,
+      y: screen.height - (30 + tankSprite.height),
+      width: tankSprite.width,
+      height: tankSprite.height
+    };
+    cities = new City(tank, citySprite);
+    cities.init();
+
+    bullets = [];
+    aliens = [];
+    
+    let rows = [1, 0, 0, 2, 2];
+
+    for(let i = 0; i < rows.length; ++i){
+      for(let j = 0; j < 10; ++j){
+        let alienType = rows[i];
+
+        aliens.push({
+          sprite: alienSprite[alienType],
+          x: 30 + j * 30 + [0, 4, 0][alienType],
+          y: 30 + i * 30, 
+          width: alienSprite[alienType][0].width,
+          height: alienSprite[alienType][0].height
+        });
+      }
+    }
+    main();
   }
-
-
-  /*
-  ********************************
-  *  Update Function - update()  *
-  ********************************
-  update() function helps you update the positions and check for events (collisions, bullet shots).
-  */
 
   function update(){
-
-    // Moving the tank
-    
-
-    // Restricting the tank's position to within the screen
-    
-
-    // Loop through bullets array to move each bullet
-    for(let i = 0; /* condition */; /* update */){
-      let bullet = bullets[i];
-      
-
-      // Check if the bullet goes out of screen (either from top or bottom)
-      
-
-      
-      // Check if the bullet hits the cities (bullets from aliens & player)
-      let h2 = bullet.height / 2;
-      
-      
-
-      // Check if the bullet hits the aliens (bullets from player)
-      for(let j = 0; /* condition */; /* update */){
-        let alien = aliens[j];
-        
-      }
-
-      // Check if the bullet hits the tank
-      
+    if(keyPressed.indexOf(37) != -1){
+      tank.x -= 4;
     }
-
-    // Aliens randomly shoot bullets by chance
-    
-
-    // Update the frame
-    
-
-    // Check if the frames number reach the level's frame requirement for movement
-
-    if(/* Replace this line with the correct condition */){
-      // Switch "motion" variable between 0 & 1.
-
-
-      // Move the aliens
-      
-
-      // If aliens reach the edge of screen, switch their direction and move forward for one row
-      
-
-      // If the aliens reaches the cities, game over!
-      
+    if(keyPressed.indexOf(39) != -1){
+      tank.x += 4;
     }
   }
-
-  /*
-  ****************************
-  *  Draw Function - draw()  *
-  ****************************
-  draw() function helps you display the game onto the screen.
-  */
 
   function draw(){
-    // Clear the screen
-    
+    screen.clear();
 
-    // Draw aliens
-    
+    for (let i = 0; i < aliens.length; ++i){
+      let alien = aliens[i]
+      screen.drawSprite(alien.sprite[motion], alien.x, alien.y);
+    }
 
-    // Draw bullets
-    
-
-    // Draw cities - cannot use drawSprite since city has its own canvas; use drawImage instead.
-    
-
-    // Draw tank - use drawSprite
-    
+    screen.ctx.save();
+    for (let i = 0; i < bullets.length; ++i){
+      screen.drawBullet(bullets[i]);
+    }
+    screen.ctx.restore();
+    screen.ctx.drawImage(cities.canvas, 0, cities.y);
+    screen.drawSprite(tank.sprite, tank.x, tank.y);
   }
 
-  /*
-  *************************************
-  *  Handling User's Inputs & Clicks  *
-  *************************************
-  This handles the user's inputs / clicks for controlling the game. 
-  */
+  $(window).on("keydown", function(event){
+    let key = event.which;
+    if (keyPressed.indexOf(key) == -1){
+      keyPressed.push(key);
+      if (key == 32){
+        bullets.push(new Bullet(tank.x + 10, tank.y, -8, 2, 6, "#ffffff"));
+      }
+    }
+  });
 
-  // Adding key to the keyPressed array when a key is pressed
-  
+  $(window).on("keyup", function(event){
+    let key = event.which;
+    let index = keyPressed.indexOf(key);
+    if (index != -1){
+      keyPressed.splice(index, 1);
+    }
+  });
 
-  // Removing key from the keyPressed array when a key is released
-  
-
-  // When retry button is click, restart the game.
-  
-
-  /*
-  **************************************************
-  *  Run the init function to kick start the game  *
-  **************************************************
-  This will run the init() function to load the resources, and eventually start the main loop.
-  */
-
+  $("#retry").on("click", function(){
+    init();
+    $(this).hide();
+  });
   init();
 });
